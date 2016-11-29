129bd36a7095d9e34a1e81d3ffe57baab1a92af2
ifalldev <cbatista.93@gmail.com>
2016-11-29 01:18:20 -0200

    star/end angle

--- circle-chart.js
+++ circle-chart.js
@@ -10,3 +10,40 @@ const height = 500 - margin.top - margin.bottom;
 
 const radius = width / 2;
 
+const data = [
+  { name: 'Atividade', count: 90 },
+  { name: 'Visita', count: 40 },
+  { name: 'Arroz', count: 50 },
+  { name: 'Esfirra', count: 100 },
+  { name: 'Naruto', count: 80 },
+  { name: 'KingKingz', count: 30 },
+];
+
+const arc = d3.arc()
+      .outerRadius(radius - 10)
+      .innerRadius(0)
+      .startAngle(0)
+      .endAngle(Math.PI / 2);
+
+const pie = d3.pie()
+      .sort(null)
+      .value(d => d.count);
+
+const svg = d3.select('body').append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', 'translate(' +  width / 2 + ',' + height / 2 + ')');
+
+console.log('before data', arc);
+
+data.forEach(d => {
+  console.log(d);
+  console.log(arc);
+  const g = svg.selectAll('.arc')
+      .data(pie(d))
+      .enter().append('g')
+      .attr('class', 'arc');
+
+  g.append('path').attr('d', arc).style('fill', 'blue');
+});
