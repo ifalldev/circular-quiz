02f56a359a55c0f066ca6cb8e3f5dc3bdbd6c1a7
ifalldev <cbatista.93@gmail.com>
2016-11-28 23:42:05 -0200

    first commit

---
+++ circle-chart.js
@@ -0,0 +1,12 @@
+const margin = {
+  top: 20,
+  right: 20,
+  left: 20,
+  bottom: 20,
+};
+
+const width  = 500 - margin.right - margin.left;
+const height = 500 - margin.top - margin.bottom;
+
+const radius = width / 2;
+
