999042888ff7cbb6280315209312680e504fbcca
ifalldev <cbatista.93@gmail.com>
2016-12-03 18:48:23 -0200

    color scheme

--- circle-chart.js
+++ circle-chart.js
@@ -20,8 +20,10 @@ const band = function(center, minRadius, maxRadius, bandNo) {
 const width  = 500 - margin.right - margin.left;
 const height = 500 - margin.top - margin.bottom;
 const bandSize = 100 / 6;
-
 const radius = width / 2;
+
+const multilevelChart = [];
+const color = d3.scaleOrdinal(d3.schemeCategory20);
 
 const data = [
   { id: 0, name: 'Atividade', count: bandSize },
@@ -32,28 +34,47 @@ const data = [
   { id: 5, name: 'KingKingz', count: bandSize },
 ];
 
-const color = d3.scaleOrdinal()
-      .range(['blue', 'yellow', 'red', 'green', 'indigo', 'violet']);
-
-let arc = d3.arc()
-      .outerRadius(radius - 10)
-      .innerRadius(120);
-      // .startAngle(180)
-      // .endAngle((Math.PI * 2) / 6);
-
-let pie = d3.pie()
-      .sort(null)
-      .value(d => d.count);
-
 let svg = d3.select('body').append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .append('g')
-      .attr('transform', 'translate(' +  width / 2 + ',' + height / 2 + ')');
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(' +  width / 2 + ',' + height / 2 + ')');
 
-const g = svg.selectAll('.arc')
-      .data(pie(data))
-      .enter().append('g')
-      .attr('class', 'arc');
+const setMultilevelChart = function(data) {
+  if(!data) return false;
 
-g.append('path').attr('d', arc).style('fill', d => color(d.data.id));
+  let level   = data.length;
+  let counter = 0;
+  let index   = 0;
+  let currentLevelData = [];
+  let queue = [];
+
+  for (let i = 0; i < data.length; i++) {
+    queue.push(data[i]);
+  }
+
+  while (queue.length > 0) {
+    let node = queue.shift();
+    currentLevelData.push(node);
+    level--;
+  }
+};
+
+const drawChart = function(_data) {
+  let arc = d3.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(120);
+
+  let pie = d3.pie()
+        .sort(null)
+        .value(d => d.count);
+
+  const g = svg.selectAll('.arc')
+        .data(pie(_data))
+        .enter().append('g')
+        .attr('class', 'arc');
+
+  g.append('path').attr('d', arc).style('fill', d => color(d.data.id));
+};
+
+drawChart(data);
