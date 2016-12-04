1eff6aa35b8094060e1de5e47289d36c36ad88c2
ifalldev <cbatista.93@gmail.com>
2016-12-04 10:09:15 -0200

    multilevel chart up

--- circle-chart.js
+++ circle-chart.js
@@ -10,62 +10,12 @@ const height    = 500;
 const bandSize  = 100 / 6;
 const maxRadius = width / 2;
 
-const multilevelChart = [];
+// const multilevelChart = [];
 const color = d3.scaleOrdinal(d3.schemeCategory20);
 
-const data = [
-    { "nodeData": {
-            "age": "<5",
-            "population": bandSize
-        },
-        "subData": [{
-            "nodeData": {
-                "age": "<5",
-                "population": bandSize
-            },
-            "subData": [{
-                "nodeData": {
-                    "age": "<5",
-                    "population": bandSize
-                }
-            }]
-        }]
-    },
-    { "nodeData": {
-            "age": "6",
-            "population": bandSize
-        },
-        "subData": [{
-            "nodeData": {
-                "age": "6",
-                "population": bandSize
-            },
-            "subData": [{
-                "nodeData": {
-                    "age": "6",
-                    "population": bandSize
-                }
-            }]
-        }]
-    },
-    { "nodeData": {
-            "age": "7",
-            "population": bandSize
-        },
-        "subData": [{
-            "nodeData": {
-                "age": "7",
-                "population": bandSize
-            },
-            "subData": [{
-                "nodeData": {
-                    "age": "7",
-                    "population": bandSize
-                }
-            }]
-        }]
-    },
-];
+const bandList  = ['naruto', 'gatos', 'inalador', 'latinha', 'cinzeiro', 'ipad'];
+const bandNivel = 5;
+ multilevelChart = [];
 
 let svg = d3.select('body').append('svg')
         .attr('width', width)
@@ -73,46 +23,62 @@ let svg = d3.select('body').append('svg')
         .append('g')
         .attr('transform', 'translate(' +  width / 2 + ',' + height / 2 + ')');
 
-const setMultilevelChart = function(data) {
-  if(!data) return;
-
-  let level   = data.length;
-  let counter = 0;
-  // let index   = 0;
-  let currentLevelData = [];
-  let queue = [];
-
-  for (let i = 0; i < data.length; i++) {
-    queue.push(data[i]);
-  }
-
-  while (queue.length > 0) {
-    let node = queue.shift();
-    currentLevelData.push(node);
-    level--;
-
-    if(node.subData) {
-      for(let i = 0; i < node.subData.length; i++) {
-        queue.push(node.subData[i]);
-        counter ++;
-      }
-    }
-
-    if (level < 1) {
-      level            = counter;
-      counter          = 0;
-
-      multilevelChart.push(currentLevelData);
-      currentLevelData = [];
-    }
+const setMultilevelChart = function(list) {
+  for (let i = 0; i < bandNivel; i++) {
+    const layer = [];
+    let k = 0;
+    list.forEach(j => {
+      layer.push({id: k, label: j, nivel: i + 1});
+      k++;
+    }, i);
+    // console.log(layer);
+    // console.log(i)
+    multilevelChart.push(layer);
   }
 };
+
+setMultilevelChart(bandList);
+
+
+  // if(!list) return;
+
+  // let level   = data.length;
+  // let counter = 0;
+  // // let index   = 0;
+  // let currentLevelData = [];
+  // let queue = [];
+
+  // for (let i = 0; i < data.length; i++) {
+  //   queue.push(data[i]);
+  // }
+
+  // while (queue.length > 0) {
+  //   let node = queue.shift();
+  //   currentLevelData.push(node);
+  //   level--;
+
+  //   if(node.subData) {
+  //     for(let i = 0; i < node.subData.length; i++) {
+  //       queue.push(node.subData[i]);
+  //       counter ++;
+  //     }
+  //   }
+
+  //   if (level < 1) {
+  //     level            = counter;
+  //     counter          = 0;
+
+  //     multilevelChart.push(currentLevelData);
+  //     currentLevelData = [];
+  //   }
+  // }
+// };
 
 const drawChart = function(_data, index) {
   // console.log('chart data', _data);
   let pie = d3.pie()
         .sort(null)
-        .value(d => d.nodeData.population);
+        .value(d => bandSize);
 
   let arc = d3.arc()
         .outerRadius((index + 1) * pieWidth - 1)
@@ -123,18 +89,19 @@ const drawChart = function(_data, index) {
         .enter().append('g')
         .attr('class', 'arc' + index);
 
-  g.append('path').attr('d', arc).style('fill', d => color(d.data.nodeData.age));
+  g.append('path').attr('d', arc).style('fill', d => color(d.data.id));
 
   g.append('text').attr('transform', d => 'translate(' + arc.centroid(d) + ')')
     .attr('dy', '.35em').style('text-anchor', 'middle')
-    .text(d => d.data.nodeData.age);
+    .text(d => d.data.nivel);
 };
 
-setMultilevelChart(data);
+// setMultilevelChart(bandList);
 const pieWidth  = parseInt(maxRadius / multilevelChart.length) - multilevelChart.length;
 
 MLC = multilevelChart;
 for(let i = 0; i < multilevelChart.length; i++) {
+  console.log(multilevelChart[i]);
   const _cData = multilevelChart[i];
   drawChart(_cData, i);
 }
